033de8a06d17fcf1ef3d021e25f9dc3c96ec1d0f
ros <122240254+MiaWilburn02@users.noreply.github.com>
2024-08-09 08:46:11 -0500

    Update Write a poem.cpp

--- C for Everyone Programming Fundamentals - University of California, Santa Cruz/Module 1/Write a poem.cpp
+++ C for Everyone Programming Fundamentals - University of California, Santa Cruz/Module 1/Write a poem.cpp
@@ -1,6 +1,6 @@
 #include <cstdio>
 
-int main()
+int main(void)
 {
     printf("In fields of green, where flowers bold unfold, Dance bees with wings of sheer, sunlit gold. From dawn till dusk, in buzzing, tireless flight, They kiss each bloom, a soft, fleeting delight.\n");
     return 0;
